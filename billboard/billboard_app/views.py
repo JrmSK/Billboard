9da1710d8d8a98d94b675679c0fcdad5b0fca750
JrmSK <jeremy.skornik@gmail.com>
2019-01-13 01:27:47 +0200

    first commit

---
+++ billboard/billboard_app/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .models import Messages
+import time
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return index(request)
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html", {"form": form})
+
+
+@login_required()
+def index(request):
+    date_post = time.strftime("%Y/%m/%d")
+    messages = Messages.objects.all()
+    if request.user.is_authenticated:
+        username = request.user.username
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        message = request.POST.get('message')
+        model = Messages(title=title, message=message, author=username, date=date_post)
+        model.save()
+    return render(request, 'billboard_app/index.html', {'messages': messages, 'date': date_post, 'username': username})
